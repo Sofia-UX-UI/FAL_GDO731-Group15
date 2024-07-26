5bdefdd599e33f642726f5a98b0266ec13d787fc
CloudyBen <Ben.watson2019@outlook.com>
2024-07-26 14:49:57 +0100

    Revert "Merge branch 'main' of https://github.com/Sofia-UX-UI/FAL_GDO731-Group15"
    This reverts commit 357fb4aaf5585784d56a8cdcb3540b2c65dad9dc, reversing
    changes made to 70a7aa304cb2fc3eda773ea09d2828336c8f96cb.

--- Development/LanguageGame/Assets/Scripts/Dialogue/DialogueManager.cs
+++ Development/LanguageGame/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -6,23 +6,16 @@ using Ink.Runtime;
 using UnityEngine.EventSystems; 
 
 // Code inspired by https://youtu.be/vY0Sk93YUhA?si=D1BYUhYjx0vQ22t7 's tutorial 
-//Code inspired by https://youtu.be/tVrxeUIEV9E?si=5G0IFErsbNYxoyQk 's tutorial 
 
 public class DialogueManager : MonoBehaviour
 {
-    [Header ("Parameters")]
-    [SerializeField] private float typingSpeed = 0.04f; //smaller number, faster it types
-
-    [Header("Load Globals JSON")]
-    [SerializeField] private TextAsset loadGlobalsJSON; 
-
     [Header ("Dialogue UI")]
     [SerializeField] private GameObject dialoguePanel; //to enable / disable dialogue Popup 
-    [SerializeField] private GameObject continueIcon; 
     [SerializeField] private TextMeshProUGUI dialogueText; //to change dialogue text 
     [SerializeField] private TextMeshProUGUI displayNameText; //to change Speaker name
     [SerializeField] private Animator portraitAnimator; //to change Speaker image
     private Animator layoutAnimator; 
+
 
     [Header ("Choices UI")]
     [SerializeField] private GameObject[] choices;
@@ -31,18 +24,13 @@ public class DialogueManager : MonoBehaviour
     
     private Story currentStory; //track which story 
     public bool dialogueIsPlaying {get; private set;} //Track to see if dialogue is actively playing or not. 
-    
-    private bool canContinueToNextLine = false; 
-    private Coroutine displayLineCoroutine; 
 
     private static DialogueManager instance; 
 
     private const string SPEAKER_TAG = "speaker";
     private const string PORTRAIT_TAG = "portrait";
     private const string LAYOUT_TAG = "layout"; 
-
-    private DialogueVariables dialogueVariables;
-     
+    
     // ------------------- Functions 
     private void Awake()
     {
@@ -51,8 +39,6 @@ public class DialogueManager : MonoBehaviour
             Debug.LogWarning("Found more than 1 Dialogue Manager");
         }
         instance = this; 
-
-        dialogueVariables = new DialogueVariables(loadGlobalsJSON);
     }
 
     public static DialogueManager GetInstance()
@@ -84,7 +70,7 @@ public class DialogueManager : MonoBehaviour
             return;
         }
 
-        if (canContinueToNextLine && currentStory.currentChoices.Count ==0 && InputManager.GetInstance().GetSubmitPressed())    //if space or continue is pressed it continues to the next line of dialogue 
+        if (InputManager.GetInstance().GetSubmitPressed())    //if space or continue is pressed it continues to the next line of dialogue 
         {
             ContinueStory(); 
         }
@@ -96,8 +82,6 @@ public class DialogueManager : MonoBehaviour
         dialogueIsPlaying = true; 
         dialoguePanel.SetActive(true); 
 
-        dialogueVariables.StartListening(currentStory); 
-
         //reset portrait, speaker name and layout 
         displayNameText.text = "???";
         portraitAnimator.Play("default"); 
@@ -106,12 +90,8 @@ public class DialogueManager : MonoBehaviour
         ContinueStory();  //grab next line of dialogue and continue forward. 
     }
 
-    private IEnumerator ExitDialogueMode() // Exits out of Dialogue and closes down all visual pop ups. 
+    public void ExitDialogueMode() // Exits out of Dialogue and closes down all visual pop ups. 
     {
-        yield return new WaitForSeconds(0.2f); 
-        
-        dialogueVariables.StopListening(currentStory); 
-
         dialogueIsPlaying = false; 
         dialoguePanel.SetActive(false);
         dialogueText.text = ""; //safe check to make sure its not displaying any linger text. 
@@ -121,56 +101,15 @@ public class DialogueManager : MonoBehaviour
     {
         if (currentStory.canContinue)
         {
-            //dialogueText.text = currentStory.Continue(); //will pull next line of dialogue --- replaced with typing below 
-            if (displayLineCoroutine != null)
-            {
-                StopCoroutine(displayLineCoroutine); 
-            }
-            displayLineCoroutine = StartCoroutine(DisplayLine(currentStory.Continue()));
-            
+            dialogueText.text = currentStory.Continue(); //will pull next line of dialogue 
+            DisplayChoices(); 
             //Dealing with tags 
             HandleTags(currentStory.currentTags); 
         }
         else 
         {
-            StartCoroutine(ExitDialogueMode());  //if end of dialogue redirect to close things out
+            ExitDialogueMode();  //if end of dialogue redirect to close things out
         }
-    }
-
-    private IEnumerator DisplayLine(string line)
-    {
-        dialogueText.text = ""; //empty dialogue text
-        continueIcon.SetActive(false); //hide items until done typing 
-        HideChoices(); 
-
-        canContinueToNextLine = false; 
-        bool isAddingRichTextTag = false; 
-
-        foreach (char letter in line.ToCharArray())
-        {
-           if(InputManager.GetInstance().GetSubmitPressed())
-           {
-                dialogueText.text = line; 
-                break; 
-           }
-           if (letter == '<' || isAddingRichTextTag)
-           {
-                isAddingRichTextTag = true; 
-                dialogueText.text += letter; 
-                if (letter == '>')
-                {
-                    isAddingRichTextTag = false; 
-                }
-           }
-           else
-           {
-                dialogueText.text += letter; 
-                yield return new WaitForSeconds(typingSpeed); 
-           }
-        }
-        canContinueToNextLine = true; 
-        continueIcon.SetActive(true);
-        DisplayChoices(); 
     }
 
     private void HandleTags(List<string> currentTags)
@@ -231,13 +170,6 @@ public class DialogueManager : MonoBehaviour
         StartCoroutine(SelectFirstchoice()); 
     }
 
-    private void HideChoices()
-    {
-        foreach (GameObject choiceButton in choices)
-        {
-            choiceButton.SetActive(false); 
-        }
-    }
     private IEnumerator SelectFirstchoice()
     {
         //Grabs the first choice first and waits. 
@@ -248,21 +180,7 @@ public class DialogueManager : MonoBehaviour
 
     public void MakeChoice(int choiceIndex)
     {
-        if (canContinueToNextLine)
-        {
-            currentStory.ChooseChoiceIndex(choiceIndex);
-           //ContinueStory(); 
-        }
-    }
+        currentStory.ChooseChoiceIndex(choiceIndex);
 
-   public Ink.Runtime.Object GetVariableState(string variableName) 
-    {
-        Ink.Runtime.Object variableValue = null;
-        dialogueVariables.variables.TryGetValue(variableName, out variableValue);
-        if (variableValue == null) 
-        {
-            Debug.LogWarning("Ink Variable was found to be null: " + variableName);
-        }
-        return variableValue;
     }
 }
--- Development/LanguageGame/Assets/Scripts/TriggerText.cs
+++ Development/LanguageGame/Assets/Scripts/TriggerText.cs
@@ -7,10 +7,13 @@ public class TriggerText : MonoBehaviour
    [Header("item to disable")]
    [SerializeField] private GameObject disableditem; 
    [Header("item to enable")]
-   [SerializeField] private GameObject enableditem;  
+   [SerializeField] private GameObject enableditem; 
+    
+    private bool playerInRange; 
    
  private void Awake()
    {
+    playerInRange = false;
     disableditem.SetActive(true); 
     enableditem.SetActive(false); 
    }
@@ -19,6 +22,7 @@ public class TriggerText : MonoBehaviour
    {
         if (collider.gameObject.tag == "Player")
         {
+            playerInRange = true;
             disableditem.SetActive(false);
             enableditem.SetActive(true); 
         }
