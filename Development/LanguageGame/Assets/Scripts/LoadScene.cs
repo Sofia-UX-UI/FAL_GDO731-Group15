5934677d43dd0140bded5ed52fba41467d6e6959
CloudyBen <Ben.watson2019@outlook.com>
2024-08-05 14:27:07 +0100

    Added Crossfade Transition (WIP - Disabled Currently)
    Transition does work but seems to "cover" the UI, so any button requiring a button can't be pressed when the transition is enabled. Disabled for now.

--- Development/LanguageGame/Assets/Scripts/LoadScene.cs
+++ Development/LanguageGame/Assets/Scripts/LoadScene.cs
@@ -5,28 +5,40 @@ using UnityEngine.SceneManagement;
 using FMOD.Studio; 
 
 public class LoadScene : MonoBehaviour
+
 {
+    public Animator transition;
     [Header("Area")]
     [SerializeField] private MusicTransition area; 
     [Header("Moving Area")]
     [SerializeField] private MusicTransition newArea; 
     public void LoadLevel1()
     {
+        StartCoroutine ("Transition");
         AudioManager.instance.PlayOneShot(FMODEvents.instance.ContinueButton, this.transform.position);
         AudioManager.instance.SetBackgroundMusic(newArea); 
         Debug.Log(newArea); 
         UnityEngine.SceneManagement.SceneManager.LoadScene("1_Airport"); 
     }
 
-      public void LoadLevel2()
+    public void LoadLevel2()
     {
+        StartCoroutine ("Transition");
         UnityEngine.SceneManagement.SceneManager.LoadScene("Level_Two"); 
         AudioManager.instance.SetBackgroundMusic(newArea); 
     }
-       public void LoadMenu()
+    public void LoadMenu()
     {
+        StartCoroutine ("Transition");
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu"); 
         AudioManager.instance.SetBackgroundMusic(area); 
        
+    }
+
+    IEnumerator Transition()
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(1f);
+
     }
 }
